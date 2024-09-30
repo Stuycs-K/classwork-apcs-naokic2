4857098846e678ce5efe0c2e296f7baa59b19bb5
Naoki Conning <naokic2@nycstudents.net>
2024-09-30 10:12:48 -0400

    finished besides swapRC

---
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -0,0 +1,51 @@
+public class ArrayMethods {
+
+public static void main(String[]args) {
+
+  int[] x = {1,4,6,8};
+  int[] z = {2,3,6,9};
+  System.out.println(arrayToString(x));
+  System.out.println(arrayToString(z));
+  int[][] testcase = {{2,3,4}, {4,3,2}, {33,23,42}};
+  System.out.println(arrToString(testcase));
+  System.out.println(arr2DSum(testcase));
+
+}
+
+
+
+public static String arrayToString(int[] arr) {
+  String x = "[";
+  for(int i=0;i<arr.length-1;i++) {
+    x += arr[i];
+    x+= ", ";
+  }
+  x = x + arr[arr.length - 1] + "]";
+  return x;
+}
+
+public static String arrToString(int[][] arr)
+{
+    String finale = "[";
+  for(int i = 0; i < arr.length - 1; i++)
+
+  {
+    finale += arrayToString(arr[i]) + ", ";
+
+  }
+
+  finale += arrayToString(arr[arr.length-1]) + "]";
+  return finale;
+
+}
+
+public static int arr2DSum(int [][]nums) {
+  int sum =0;
+  for(int i=0; i <nums.length;i++) {
+    for(int x=0; x <nums[i].length;x++) {
+      sum += nums[i][x];
+    }
+  }
+  return sum;
+}
+}
