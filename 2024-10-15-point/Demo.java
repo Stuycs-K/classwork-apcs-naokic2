d700d201677399fa1926225e5205bce5c07fea22
Naoki Conning <naokic2@nycstudents.net>
2024-10-16 04:17:46 -0700

    copied work from partner, finished in class

---
+++ 2024-10-15-point/Demo.java
@@ -0,0 +1,30 @@
+import java.lang.Math;
+public class Demo{
+
+  /*
+  Write this method third. Test it then move on
+  to the others.
+  */
+  public static double distance(Point a, Point b){
+    return Math.sqrt(Math.pow((b.getX() - a.getX()), 2) + Math.pow((b.getY() - a.getY()), 2));
+  }
+
+  public static void main(String[]args){
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(-1,-1);
+    Point p3 = new Point(3,4);
+    Point p4 = new Point(p3);
+    System.out.println( p3);
+    System.out.println( p4);
+    System.out.println( distance(p1,p2));
+    System.out.println( Point.distance(p1,p2));
+    System.out.println( p1.distanceTo(p2));
+    Point tri1 = new Point(0,0);
+    Point tri2 = new Point(1,0);
+    Point tri3 = new Point(0.5,Math.sqrt(0.75));
+    System.out.println( tri1.distanceTo(tri2));
+    System.out.println( tri1.distanceTo(tri3));
+    System.out.println( tri2.distanceTo(tri3));
+
+  }
+}
---
+++ 2024-10-15-point/Point.java
@@ -0,0 +1,45 @@
+import java.lang.Math;
+public class Point{
+  private double x,y;
+
+  public String toString(){
+    return "(" + x + ", " + y + ")";
+  }
+
+  //Initialize this Point to have the same values as the other Point.
+  public Point(Point other){
+    this.x = other.x;
+    this.y = other.y;
+    //COMPLETE THIS SECOND
+    //Then write test cases.
+  }
+
+  //Initialize this Point to have the provided values
+  public Point(double x, double y){
+    this.x=x;
+    this.y=y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+  /*
+  Write this method FOURTH, go to the main and do that one first.
+  */
+  public double distanceTo(Point other){
+    double distance = 0;
+    distance = Math.sqrt(Math.pow((other.x - this.x), 2) + Math.pow((other.y - this.y), 2));
+    return distance;
+  }
+
+  /*
+  Write this method last. Re-use (NOT COPY/PASTE) prior work and
+  do not write redundant code.
+  */
+  public static double distance(Point a, Point b){
+    return Math.sqrt(Math.pow((b.x - a.x), 2) + Math.pow((b.y - a.y),2));
+  }
+}
