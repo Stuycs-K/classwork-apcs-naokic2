362993724a23533f1f59b6a8f24f07cec465373a
Naoki Conning <naokic2@nycstudents.net>
2024-11-07 10:15:16 -0500

    ended period

--- 2024-11-07-Pets/Animal.java
+++ 2024-11-07-Pets/Animal.java
@@ -4,7 +4,9 @@ public class Animal {
   private String name;
 
   public Animal(String noise, int age, String name){
-    //initialize the values (hint use this.varname to clarify)
+    this.noise = noise;
+    this.age = age;
+    this.name = name;
   }
 
   //2.
@@ -17,7 +19,10 @@ public class Animal {
   Note: you MUST use getName() getNoise() here! You will see why later.
   */
   public void speak(){
-
+    System.out.println("My name is " + getName() + ".");
+    System.out.println("I am " + getAge() + " years old.");
+    System.out.println("I say " + getNoise() + ".");
+    
   }
 
   public String getName(){
@@ -25,5 +30,8 @@ public class Animal {
   }
   public String getNoise(){
     return noise;
+  }
+  public int getAge(){
+    return age;
   }
 }
--- 2024-11-07-Pets/Bird.java
+++ 2024-11-07-Pets/Bird.java
@@ -8,7 +8,9 @@ public class Bird extends Animal {
     //In order to construct the Bird you must first call the superclass constructor
     //This is done by saying: super(parameters)
     //e.g. super("Doug",10,"Squaaaak!");
-
+    super(noise, age, name);
+    this.height = height;
+    this.color = color;
   }
 
 
@@ -22,7 +24,12 @@ public class Bird extends Animal {
     I am ? inches tall
   */
   //You can call the old method using super.speak();
-  //public void speak(){}
+  public void speak(){
+    super.speak();
+    System.out.println("My feathers are colored " + color + ".");
+    System.out.println("I am " + height + " inches tall.");
+  }
+
 
 
 
@@ -32,7 +39,9 @@ public class Bird extends Animal {
 
   //Step 8:In Driver.java answer: What is the difference before and after you override this method?
   //What does this tell us about methods that are overridden?
-  //public String getName(){  }
+  public String getName(){
+    return("The Mighty " + super.getName());
+  }
 
   //Go back and complete steps 9 onwards.
 
