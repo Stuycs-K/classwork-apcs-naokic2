7bcc8bc17e720ab31efcb5ec568ca178421ba632
Naoki Conning <naokic2@nycstudents.net>
2024-12-06 10:15:05 -0500

    end of pd

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -30,7 +30,8 @@ public class Wizard extends Adventurer{
   }
   //hurt or hinder the target adventurer
   public abstract String attack(Adventurer other){
-
+    other.applyDamage(1);
+    return "Used attack! dealt 1 damage!"
   }
 
   //heall or buff the target adventurer
@@ -45,7 +46,13 @@ public class Wizard extends Adventurer{
 
   //hurt or hinder the target adventurer, consume some special resource
   public abstract String specialAttack(Adventurer other){
-    
+    if(HogwartsPoints >= 10){
+      other.applyDamage(3);
+      HogwartsPoints -= 10
+      return "Used special attack! dealt 3 damage, but consumed 10 Hogwarts Points! You have: " + HogwartsPoints + " many points left."
+    } else {
+      return "Insufficient HogwartsPoints."
+    }
   }
 
 
